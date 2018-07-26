fcff4b23312f34633698119db08b95a8334be192
100gold <laex.ru@gmail.com>
2018-07-26 17:10:09 +0300

    x86 fix; static runtime; proxybuilder explanation

--- test_targetdll/lib.cpp
+++ test_targetdll/lib.cpp
@@ -3,6 +3,23 @@
 
 #include <Windows.h>
 
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	OutputDebugStringA("!!!!dll loaded!!!\n");
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
 void function_1(int a, int b)
 {
   MessageBoxA(0, "FUNCTION1", "F1", 0);
